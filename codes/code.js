1b66683fbc4fbe0eb48e866874fb12d27cd4232a
Agneay B Nair <agneaybnair@gmail.com>
2025-09-13 10:47:06 +0530

    created basic project structure with some basic code

---
+++ codes/code.js
@@ -0,0 +1,30 @@
+async function searchPapers() {
+    let query = document.getElementById("searchBox").value.toLowerCase();
+    let filter = document.getElementById("filterType").value;
+    let resultList = document.getElementById("results");
+    resultList.innerHTML = "Loading....";
+
+    let response = await fetch("./data/papers.json");
+    let papers = await response.json();
+
+    let filtered = papers.filter(paper => {
+        return (
+            paper.courseCode.toLowerCase().includes(query) &&
+            (filter === "" || paper.type === filter)
+        );
+    });
+    resultList.innerHTML = "";
+    if (filtered.length === 0) {
+        resultList.innerHTML = "<li>No Papers Found</li>";
+    } else {
+        filtered.forEach(paper => {
+            let li = document.createElement("li");
+            li.innerHTML = `<a href="${paper.link}" target="_blank">${paper.title}</a>`;
+            resultList.appendChild(li);
+        });
+    }
+
+}
+
+let now = new Date();
+document.getElementById("time").textContent = now.toLocaleTimeString();
